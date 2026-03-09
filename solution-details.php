5c126fa4d98352c32e8d91d748a72c495efa7cf8
GKDevotion <gk@devotiontech.io>
2026-03-09 10:41:16 +0530

    add service solutions

---
+++ solution-details.php
@@ -0,0 +1,122 @@
+<?php
+include_once('elements/header.php');
+?>
+
+<div class="breadcumb-wrapper" data-bg-src="assets/img/bg/breadcumb-bg.jpg">
+    <div class="container">
+        <div class="breadcumb-content">
+
+            <h1 class="breadcumb-title" id="serviceTitle"></h1>
+
+            <ul class="breadcumb-menu">
+                <li>
+                    <a href="index.php">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="services.php">
+                        Solutions
+                    </a>
+                </li>
+                <li id="serviceTitle2"></li>
+            </ul>
+
+        </div>
+    </div>
+</div>
+
+<section class="premium-services">
+
+    <div class="container">
+
+        <div class="text-center mt-5">
+            <h2 class="section-heading" id="serviceTitle3"></h2>
+            <p class="section-sub" id="serviceDescription"></p>
+        </div>
+
+        <div class="row gy-4 mb-5" id="serviceList"></div>
+
+    </div>
+
+</section>
+
+<?php
+include_once('elements/contact-us.php');
+include_once('elements/footer.php');
+?>
+
+
+<script>
+    const params = window.location.pathname;
+    let service = params.split("/");
+
+    const count = service.length;
+
+    if(count > 3){
+        service = service[4];
+    }else{
+        service = service[2];
+    }
+
+    fetch("assets/data/solutions.json")
+    .then(res => res.json())
+    .then(data => {
+
+        const serviceData = data[service];
+
+        if (!serviceData) {
+            console.log("Service not found");
+            return;
+        }
+
+        // breadcrumb
+        document.getElementById("serviceTitle").innerText = serviceData.title;
+        document.getElementById("serviceTitle2").innerText = serviceData.title;
+
+        // section
+        document.getElementById("serviceTitle3").innerText = serviceData.title;
+        document.getElementById("serviceDescription").innerText = serviceData.description;
+
+        let html = "";
+
+        serviceData.services.forEach((item, index) => {
+
+        let number = (index + 1).toString().padStart(1, '0');
+
+        html += `
+        <div class="col-md-6 col-xl-4">
+        <div class="service-card">
+
+        <div class="service-card_number">${number}</div>
+
+        <div class="shape-icon">
+            <img src="assets/img/icon/${item.icon}" alt="Icon">
+            <span class="dots"></span>
+        </div>
+
+        <h3 class="box-title">
+            <a href="${item.link}">
+                ${item.title}
+            </a>
+        </h3>
+
+        <p class="service-card_text">
+            ${item.text}
+        </p>
+
+        <div class="bg-shape">
+            <img src="assets/img/bg/service_card_bg.png" alt="bg">
+        </div>
+
+            </div>
+        </div>
+        `;
+
+        });
+
+        document.getElementById("serviceList").innerHTML = html;
+
+    })
+    .catch(error => console.log(error));
+</script>
