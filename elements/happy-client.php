40de1e58ee29793a6fd44dd49dccdfe55882250e
hardik prajapati <hardikprajapati8104@gmail.com>
2026-03-10 10:14:21 +0530

    update happy client reviews fetch data from json file

--- elements/happy-client.php
+++ elements/happy-client.php
@@ -15,146 +15,56 @@
         <div class="slider-area">
             <div class="swiper th-slider has-shadow" id="testiSlider2"
                 data-slider-options='{"loop":true,"breakpoints":{"0":{"slidesPerView":1},"576":{"slidesPerView":"1"},"768":{"slidesPerView":"1"},"992":{"slidesPerView":"1"},"1200":{"slidesPerView":"2"}}}'>
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="testi-box">
-                            <div class="testi-box_img">
-                                <img src="assets/img/testimonial/testi_2_1.jpg" alt="Avater">
-                                <div class="testi-box_quote">
-                                    <img src="assets/img/icon/quote_left_2.svg" alt="quote">
-                                </div>
-                            </div>
-                            <div class="testi-box_content">
-                                <p class="testi-box_text">
-                                    Objectively visualize error-free technology for B2B
-                                    alignment. Monotonectally harness an expanded array of models via effective
-                                    collaboration.
-                                </p>
-                                <div class="testi-box_review">
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                </div>
-                                <h3 class="box-title">David Farnandes</h3>
-                                <p class="testi-box_desig">CEO at Anaton</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="testi-box">
-                            <div class="testi-box_img">
-                                <img src="assets/img/testimonial/testi_2_2.jpg" alt="Avater">
-                                <div class="testi-box_quote">
-                                    <img src="assets/img/icon/quote_left_2.svg" alt="quote">
-                                </div>
-                            </div>
-                            <div class="testi-box_content">
-                                <p class="testi-box_text">
-                                    Objectively visualize error-free technology for B2B
-                                    alignment. Monotonectally harness an expanded array of models via effective
-                                    collaboration.
-                                </p>
-                                <div class="testi-box_review">
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                    <i class="fa-solid fa-star-sharp"></i>
-                                </div>
-                                <h3 class="box-title">
-                                    Jackline Techie
-                                </h3>
-                                <p class="testi-box_desig">
-                                    CEO at Kormola
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="testi-box">
-                            <div class="testi-box_img"><img src="assets/img/testimonial/testi_2_3.jpg" alt="Avater">
-                                <div class="testi-box_quote"><img src="assets/img/icon/quote_left_2.svg"
-                                        alt="quote"></div>
-                            </div>
-                            <div class="testi-box_content">
-                                <p class="testi-box_text">Objectively visualize error-free technology for B2B
-                                    alignment. Monotonectally harness an expanded array of models via effective
-                                    collaboration.</p>
-                                <div class="testi-box_review"><i class="fa-solid fa-star-sharp"></i><i
-                                        class="fa-solid fa-star-sharp"></i><i class="fa-solid fa-star-sharp"></i><i
-                                        class="fa-solid fa-star-sharp"></i><i class="fa-solid fa-star-sharp"></i>
-                                </div>
-                                <h3 class="box-title">Abraham Khalil</h3>
-                                <p class="testi-box_desig">CEO at Rimasu</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="testi-box">
-                            <div class="testi-box_img"><img src="assets/img/testimonial/testi_2_1.jpg" alt="Avater">
-                                <div class="testi-box_quote"><img src="assets/img/icon/quote_left_2.svg"
-                                        alt="quote"></div>
-                            </div>
-                            <div class="testi-box_content">
-                                <p class="testi-box_text">Objectively visualize error-free technology for B2B
-                                    alignment. Monotonectally harness an expanded array of models via effective
-                                    collaboration.</p>
-                                <div class="testi-box_review"><i class="fa-solid fa-star-sharp"></i><i
-                                        class="fa-solid fa-star-sharp"></i><i class="fa-solid fa-star-sharp"></i><i
-                                        class="fa-solid fa-star-sharp"></i><i class="fa-solid fa-star-sharp"></i>
-                                </div>
-                                <h3 class="box-title">David Farnandes</h3>
-                                <p class="testi-box_desig">CEO at Anaton</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="testi-box">
-                            <div class="testi-box_img"><img src="assets/img/testimonial/testi_2_2.jpg" alt="Avater">
-                                <div class="testi-box_quote"><img src="assets/img/icon/quote_left_2.svg"
-                                        alt="quote"></div>
-                            </div>
-                            <div class="testi-box_content">
-                                <p class="testi-box_text">Objectively visualize error-free technology for B2B
-                                    alignment. Monotonectally harness an expanded array of models via effective
-                                    collaboration.</p>
-                                <div class="testi-box_review"><i class="fa-solid fa-star-sharp"></i><i
-                                        class="fa-solid fa-star-sharp"></i><i class="fa-solid fa-star-sharp"></i><i
-                                        class="fa-solid fa-star-sharp"></i><i class="fa-solid fa-star-sharp"></i>
-                                </div>
-                                <h3 class="box-title">Jackline Techie</h3>
-                                <p class="testi-box_desig">CEO at Kormola</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="testi-box">
-                            <div class="testi-box_img"><img src="assets/img/testimonial/testi_2_3.jpg" alt="Avater">
-                                <div class="testi-box_quote"><img src="assets/img/icon/quote_left_2.svg"
-                                        alt="quote"></div>
-                            </div>
-                            <div class="testi-box_content">
-                                <p class="testi-box_text">Objectively visualize error-free technology for B2B
-                                    alignment. Monotonectally harness an expanded array of models via effective
-                                    collaboration.</p>
-                                <div class="testi-box_review"><i class="fa-solid fa-star-sharp"></i><i
-                                        class="fa-solid fa-star-sharp"></i><i class="fa-solid fa-star-sharp"></i><i
-                                        class="fa-solid fa-star-sharp"></i><i class="fa-solid fa-star-sharp"></i>
-                                </div>
-                                <h3 class="box-title">Abraham Khalil</h3>
-                                <p class="testi-box_desig">CEO at Rimasu</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+                <div class="swiper-wrapper" id="testimonialContainer"></div>
+
             </div>
+
             <button data-slider-prev="#testiSlider2" class="slider-arrow style3 slider-prev">
                 <i class="far fa-arrow-left"></i></button> <button data-slider-next="#testiSlider2" class="slider-arrow style3 slider-next"><i class="far fa-arrow-right"></i>
             </button>
+
         </div>
     </div>
+    <script>
+        fetch("assets/data/testimonials.json")
+        .then(response => response.json())
+        .then(data => {
+
+            let container = document.getElementById("testimonialContainer");
+            let html = "";
+
+            data.testimonials.forEach(item => {
+
+                let stars = "";
+                for(let i = 0; i < item.rating; i++){
+                    stars += `<i class="fa-solid fa-star-sharp"></i>`;
+                }
+
+                html += `
+                <div class="swiper-slide">
+                    <div class="testi-box">
+                        <div class="testi-box_img">
+                            <img src="${item.image}" alt="Avatar">
+                            <div class="testi-box_quote">
+                                <img src="assets/img/icon/quote_left_2.svg" alt="quote">
+                            </div>
+                        </div>
+                        <div class="testi-box_content">
+                            <p class="testi-box_text">${item.text}</p>
+                            <div class="testi-box_review">${stars}</div>
+                            <h3 class="box-title">${item.name}</h3>
+                            <p class="testi-box_desig">${item.designation}</p>
+                        </div>
+                    </div>
+                </div>
+                `;
+            });
+
+            container.innerHTML = html;
+        });
+</script>
+
     <div class="shape-mockup moving d-none d-xl-block" data-bottom="0%" data-left="10%">
         <img src="assets/img/shape/line_1.png" alt="shape">
     </div>
